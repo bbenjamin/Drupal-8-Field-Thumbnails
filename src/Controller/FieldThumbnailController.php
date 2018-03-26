cbd7db30bb9b852992144f9758cf522330127b51
Ben Mullins <benm@umich.edu>
2018-03-25 23:23:14 -0400

    update to meet coding standards

--- src/Controller/FieldThumbnailController.php
+++ src/Controller/FieldThumbnailController.php
@@ -4,6 +4,7 @@ namespace Drupal\field_thumbnail\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Component\Utility\Xss;
+use Drupal\file\Entity\File;
 
 /**
  * Class FieldThumbnailController.
@@ -14,20 +15,19 @@ class FieldThumbnailController extends ControllerBase {
    * Fieldthumbmake.
    *
    * @return array
-   *
+   *   Render array to create modal.
    */
-
   public function fieldThumbMake($fid) {
     $return = [];
-    $field_config =  \Drupal::entityTypeManager()->getStorage('field_config')->load($fid);
+    $field_config = \Drupal::entityTypeManager()->getStorage('field_config')->load($fid);
     $title = $field_config->label();
     $thumb_fid = $field_config->getThirdPartySetting('field_thumbnail', 'field_config_thumbnail');
     $thumb_desc = $field_config->getThirdPartySetting('field_thumbnail', 'field_config_thumbnail_description');
     $field_description = $field_config->getDescription();
 
-    $file = \Drupal\file\Entity\File::load($thumb_fid[0]);
+    $file = File::load($thumb_fid[0]);
 
-    if($file) {
+    if ($file) {
       $return['thumb_image'] = [
         '#theme' => 'image_style',
         '#uri' => $file->getFileUri(),
@@ -37,34 +37,44 @@ class FieldThumbnailController extends ControllerBase {
       ];
     }
 
-    if(!empty($thumb_desc)) {
+    if (!empty($thumb_desc)) {
       $return['thumb_description'] = [
         '#markup' => Xss::filter($thumb_desc),
         '#prefix' => '<p><b>',
-        '#suffix' => '</b></p>'
+        '#suffix' => '</b></p>',
       ];
     }
 
-    if(!empty($field_description)) {
+    if (!empty($field_description)) {
       $return['field_description'] = [
         '#markup' => $field_description,
         '#prefix' => '<p><i>',
-        '#suffix' => '</i></p>'
+        '#suffix' => '</i></p>',
       ];
     }
 
-    if(!empty($return)) {
+    if (!empty($return)) {
       return $return;
-    } else {
+    }
+    else {
       return [
         '#type' => 'markup',
-        '#markup' => t('there was a problem retrieving the field data')
+        '#markup' => t('there was a problem retrieving the field data'),
       ];
     }
   }
 
-  public function title($fid){
-    $field_config =  \Drupal::entityTypeManager()->getStorage('field_config')->load($fid);
+  /**
+   * Generate the modal title from the field name.
+   *
+   * @param int $fid
+   *   The field id.
+   *
+   * @return \Drupal\Core\StringTranslation\TranslatableMarkup
+   *   Returns Translatable String.
+   */
+  public function title($fid) {
+    $field_config = \Drupal::entityTypeManager()->getStorage('field_config')->load($fid);
     return t('Preview: @field', ['@field' => $field_config->label()]);
   }
 }
